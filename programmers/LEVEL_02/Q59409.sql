18cfd45711d2b179076c9ebc616d9a8fa343b0a1
hylim <ivorymood@gmail.com>
2021-08-05 17:05:06 +0900

    중성화 여부 파악하기

---
+++ programmers/LEVEL_02/Q59409.sql
@@ -0,0 +1,10 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59409
+-- 중성화 여부 파악하기
+
+SELECT ANIMAL_ID, 
+        NAME, 
+        IF(SEX_UPON_INTAKE LIKE "%Neutered%" 
+            OR SEX_UPON_INTAKE LIKE "%Spayed%"
+          , "O", "X") "중성화"
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
