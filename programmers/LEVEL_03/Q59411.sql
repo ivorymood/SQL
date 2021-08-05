664e67ff185e943701dcc8ebd974351575afe2ae
hylim <ivorymood@gmail.com>
2021-08-05 17:37:45 +0900

    오랜 기간 보호한 동물(2)

---
+++ programmers/LEVEL_03/Q59411.sql
@@ -0,0 +1,8 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59411
+-- 오랜 기간 보호한 동물(2)
+
+SELECT AO.ANIMAL_ID, AO.NAME
+FROM ANIMAL_OUTS AO 
+JOIN ANIMAL_INS AI ON AO.ANIMAL_ID = AI.ANIMAL_ID
+ORDER BY DATEDIFF(AI.DATETIME, AO.DATETIME)
+LIMIT 2;
